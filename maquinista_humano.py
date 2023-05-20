629c34562a01ff8f5edae3b5c5867da0dc8cd749
JeiversonChristian <jeiversonc@gmail.com>
2023-05-19 22:24:35 -0300

    adicionei a imagem do trem bala de forma estática

--- maquinista_humano.py
+++ maquinista_humano.py
@@ -5,10 +5,15 @@ LARGURA_TELA = 1200
 ALTURA_TELA = 350
 
 FUNDO_IMG = pygame.image.load('imgs/trem bala fundo.jpg')
+TREM_BALA_IMG = pygame.image.load('imgs/trem bala.png')
+
+COMPRIMENTO_TREM = TREM_BALA_IMG.get_width()
+ALTURA_TREM = TREM_BALA_IMG.get_height()
 
 def desenhar_tela(tela):
 
     tela.blit(FUNDO_IMG, (0,0))
+    tela.blit(TREM_BALA_IMG, (LARGURA_TELA - COMPRIMENTO_TREM, ALTURA_TELA - ALTURA_TREM - 10))
     pygame.display.update()
 
 def main():
