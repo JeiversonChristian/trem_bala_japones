97d336f290b70bcbbeb8b839a5baa59dde3302ce
JeiversonChristian <jeiversonc@gmail.com>
2023-05-19 21:58:05 -0300

    adicionei a imagem de fundo

--- maquinista_humano.py
+++ maquinista_humano.py
@@ -4,7 +4,11 @@ import sys
 LARGURA_TELA = 1200
 ALTURA_TELA = 350
 
-def desenhar_tela():
+FUNDO_IMG = pygame.image.load('imgs/trem bala fundo.jpg')
+
+def desenhar_tela(tela):
+
+    tela.blit(FUNDO_IMG, (0,0))
     pygame.display.update()
 
 def main():
@@ -17,6 +21,6 @@ def main():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        desenhar_tela()
+        desenhar_tela(tela)
 
 main()
