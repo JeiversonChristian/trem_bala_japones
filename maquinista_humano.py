090c08ac3b3e25c5fee4a3f4b440b6c8c7014842
JeiversonChristian <jeiversonc@gmail.com>
2023-05-21 16:06:07 -0300

    comentando o código

--- maquinista_humano.py
+++ maquinista_humano.py
@@ -1,5 +1,12 @@
-import pygame
-import sys
+#---------------------------------------------------------------------------------------------------------
+# bibliotecas
+
+import pygame # usada para gerar e manipular as imagens
+import sys # usada simplismente para interromper todo o programa
+#---------------------------------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------------
+# constantes
 
 LARGURA_TELA = 1200
 ALTURA_TELA = 350
@@ -9,11 +16,19 @@ TREM_BALA_IMG = pygame.image.load('imgs/trem bala.png')
 
 COMPRIMENTO_TREM = TREM_BALA_IMG.get_width()
 ALTURA_TREM = TREM_BALA_IMG.get_height()
+#---------------------------------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------------
+# funções
 
 def desenhar_tela(tela):
 
+    # blit -  "bit block transfer" (transferência de blocos de bits)
+    # cópia de uma região de pixels de uma imagem para outra
     tela.blit(FUNDO_IMG, (0,0))
     tela.blit(TREM_BALA_IMG, (LARGURA_TELA - COMPRIMENTO_TREM, ALTURA_TELA - ALTURA_TREM - 10))
+
+    # atualiza a tela
     pygame.display.update()
 
 def main():
@@ -27,5 +42,6 @@ def main():
                 sys.exit()
 
         desenhar_tela(tela)
+#---------------------------------------------------------------------------------------------------------
 
 main()
