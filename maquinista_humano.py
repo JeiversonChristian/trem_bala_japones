3c65d79fcc9e3ade23fbf49bb20a39231dfe691f
JeiversonChristian <jeiversonc@gmail.com>
2023-05-21 17:06:27 -0300

    criando a classe do trem e mudando a forma de desenhá-lo

--- maquinista_humano.py
+++ maquinista_humano.py
@@ -19,15 +19,39 @@ ALTURA_TREM = TREM_BALA_IMG.get_height()
 #---------------------------------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------------------------------
+# classes
+
+class trem_bala:
+
+    # x, y -> posições inicias do trem
+    def __init__(self, x):
+        self.img = TREM_BALA_IMG
+        self.comprimento = COMPRIMENTO_TREM
+        self.altura = ALTURA_TREM
+        self.x = x
+        self.y = ALTURA_TELA - self.altura - 10
+        #self.numero = numero
+        #self.geracao = geracao
+        #self.inputs = []
+        #self.pesos = []
+        #self.bias = 0
+        #self.output = 0
+
+    def desenhar(self,tela):
+        tela.blit(self.img, (self.x, self.y))
+#---------------------------------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------------
 # funções
 
-def desenhar_tela(tela):
+def desenhar_tela(tela, trem_bala):
 
     # blit -  "bit block transfer" (transferência de blocos de bits)
     # cópia de uma região de pixels de uma imagem para outra
     # (0,0) -> onde "começa"
     tela.blit(FUNDO_IMG, (0,0))
-    tela.blit(TREM_BALA_IMG, (LARGURA_TELA - COMPRIMENTO_TREM, ALTURA_TELA - ALTURA_TREM - 10))
+
+    trem_bala.desenhar(tela)
 
     # atualiza a tela
     pygame.display.update()
@@ -37,6 +61,8 @@ def main():
     # define a tela do jogo com as dimensões passadas
     tela = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
 
+    trem_bala1 = trem_bala(0)
+
     while True:
 
         # verifica se cliquei no X para fechar o jogo
@@ -45,7 +71,7 @@ def main():
                 # encerra todo o código
                 sys.exit()
 
-        desenhar_tela(tela)
+        desenhar_tela(tela, trem_bala1)
 #---------------------------------------------------------------------------------------------------------
 
 main()
